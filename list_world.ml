ae4ecda60c5e6899063c84e55b27bf5268c0a911
sfritz <scottgfritz@gmail.com>
2014-09-07 19:46:13 -0500

    Some extra functions

--- list_world.ml
+++ list_world.ml
@@ -38,28 +38,49 @@ let live_neighbors grid x y : int =
       | Some is_alive -> is_alive
     )
 
-let next grid : t =
+let mapi grid ~f =
   List.mapi grid ~f:(fun y row ->
     List.mapi row ~f:(fun x is_alive ->
-      let neighbors = live_neighbors grid x y in
-        match is_alive, neighbors with
-        | true, n  when n < 2          -> false
-        | true, n  when n = 2 || n = 3 -> true
-        | true, n  when n > 3          -> false
-        | true, _ -> assert false
-        | false, n when n = 3          -> true
-        | false, _                     -> false
+      f x y is_alive
     )
   )
 
-let to_string grid : string =
-  String.concat ~sep:"\n"
-    (List.map grid ~f:(fun row ->
-      String.concat ~sep:""
-        (List.map row ~f:(fun is_alive ->
-          if is_alive then "X" else " "
-        ))
+let next grid : t =
+  mapi grid ~f:(fun x y is_alive ->
+    let neighbors = live_neighbors grid x y in
+      match is_alive, neighbors with
+      | true, n  when n < 2          -> false
+      | true, n  when n = 2 || n = 3 -> true
+      | true, n  when n > 3          -> false
+      | true, _ -> assert false
+      | false, n when n = 3          -> true
+      | false, _                     -> false
+  )
+
+let to_state grid : state =
+  List.concat
+    (List.mapi grid ~f:(fun y row ->
+      List.filter_mapi row ~f:(fun x is_alive ->
+        match is_alive with
+        | false -> None
+        | true -> Some (x, y)
+      )
     ))
+
+
+let to_string grid : string =
+  String.concat [
+    "\n";
+    (String.concat ~sep:"\n"
+      (List.map grid ~f:(fun row ->
+        String.concat ~sep:""
+          (List.map row ~f:(fun is_alive ->
+            if is_alive then "X" else " "
+          ))
+      ))
+    );
+    "\n"
+  ]
 
 let print grid : unit =
   printf "%s" (to_string grid);
@@ -83,7 +104,7 @@ let iterations x =
   done
 
 let main () =
-  let world = make (grid 5 Patterns.blinker) in
+  let world = make (grid 5 Patterns.toad) in
     while true do
       print (world ())
    done
