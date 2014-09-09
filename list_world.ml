f766d83d4ec90c99016a5491729f24c7dabfd2cb
sfritz <scottgfritz@gmail.com>
2014-09-08 21:31:23 -0500

    Factor out some functions

--- list_world.ml
+++ list_world.ml
@@ -1,27 +1,23 @@
 open Core.Std
 
-type t = bool list list
+(* polymorphic guys *)
 
-type state = (int * int) list
-
-let grid size (initial: state) : t =
+let init size ~f:(f : int -> int -> 'a ) : 'b =
   List.init size ~f:(fun y ->
-    List.init size ~f:(fun x ->
-      List.mem initial (x, y)
-    )
+    List.init size ~f:(fun x -> f x y)
   )
 
 let mod' x y =
   (x mod y + y) mod y
 
-let get grid x y : bool option =
+let get (grid: 'a list list) x y : 'a option =
   (* assumes square grid *)
   let length = List.length grid in
   match List.nth grid (mod' y length) with
   | None -> Printf.printf "%d, %d" x y; assert false
   | Some row -> List.nth row (mod' x length)
 
-let live_neighbors grid x y : int =
+let get_neighbors (grid: 'a list list) (x: int) (y: int) =
   [
     get grid (x-1) (y-1);
     get grid (x)   (y-1);
@@ -37,20 +33,56 @@ let live_neighbors grid x y : int =
         | None -> false
         | Some _ -> true
      )
- |> List.count ~f:(fun cell ->
-      match cell with
-      | None -> assert false
-      | Some is_alive -> is_alive
-    )
 
-let mapi grid ~f =
+let mapi grid ~f:(f: int -> int -> 'a) =
   List.mapi grid ~f:(fun y row ->
-    List.mapi row ~f:(fun x is_alive ->
-      f x y is_alive
+    List.mapi row ~f:(fun x cell ->
+      f x y cell
     )
   )
 
-let next grid : t =
+let flat_filter_mapi (grid: 'a list list) ~f:(f: int -> int -> 'a -> 'b option): ('b list)  =
+  (* List.join here? *)
+  List.concat
+    (List.mapi grid ~f:(fun y row ->
+      List.filter_mapi row ~f:(fun x is_alive ->
+        match is_alive with
+        | false -> None
+        | true -> Some (x, y)
+      )
+    ))
+
+let to_string (grid: 'a list list) ~f:(f: 'a -> string) : string =
+  String.concat ~sep:"\n"
+    (List.map grid ~f:(fun row ->
+      String.concat ~sep:"" (List.map row ~f:f)
+    ))
+
+let thunker x ~f =
+  let state = ref x in
+  fun () ->
+    let next = f !state in
+    state := next;
+    !state
+
+(* implementation *)
+
+type cell = bool
+type t = cell list list
+type state = (int * int) list
+
+let grid (size: int) (initial: state) : t =
+  init size ~f:(fun x y -> List.mem initial (x, y))
+
+let live_neighbors (grid: t) (x: int) (y: int) : int =
+  get_neighbors grid x y
+  |> List.count ~f:(fun cell ->
+       match cell with
+       | None -> assert false
+       | Some is_alive -> is_alive
+     )
+
+let next (grid: t) : t =
   mapi grid ~f:(fun x y is_alive ->
     let neighbors = live_neighbors grid x y in
       match is_alive, neighbors with
@@ -62,41 +94,21 @@ let next grid : t =
       | false, _                     -> false
   )
 
-let to_state grid : state =
-  List.concat
-    (List.mapi grid ~f:(fun y row ->
-      List.filter_mapi row ~f:(fun x is_alive ->
-        match is_alive with
-        | false -> None
-        | true -> Some (x, y)
-      )
-    ))
+let to_state (grid: t) : state =
+ flat_filter_mapi grid ~f:(fun x y is_alive ->
+   match is_alive with
+   | false -> None
+   | true -> Some (x, y)
+ )
 
+let to_string (grid: t) : string =
+  to_string grid ~f:(fun is_alive ->
+    if is_alive then "X" else " "
+  )
 
-let to_string grid : string =
-  String.concat [
-    "\n";
-    (String.concat ~sep:"\n"
-      (List.map grid ~f:(fun row ->
-        String.concat ~sep:""
-          (List.map row ~f:(fun is_alive ->
-            if is_alive then "X" else " "
-          ))
-      ))
-    );
-    "\n"
-  ]
-
-let print grid : unit =
-  printf "%s" (to_string grid);
-  printf "\n%s\n" (String.make (List.length grid) '-')
-
-let thunker x ~f =
-  let state = ref x in
-  fun () ->
-    let next = f !state in
-    state := next;
-    !state
+let print (grid: t) : unit =
+  printf "%s\n" (to_string grid);
+  printf "%s\n" (String.make (List.length grid) '-')
 
 let make grid = thunker grid ~f:next
 
